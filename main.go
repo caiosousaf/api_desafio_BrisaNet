81c9a70e105059afafa57f19d8c916b5249e3e13
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-06-07 21:21:32 -0300

    "added: function listing and creating new projects "

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+)
+
+// projeto represents data about a record projeto.
+type projeto struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Description string  `json:"Description"`
+}
+
+// projetos slice to seed record projeto data.
+var projetos = []projeto{
+    {ID: "1", Title: "Blue Train", Description: "talvez de certo"},
+    {ID: "2", Title: "Jeru", Description: "talvez de certo"},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Description: "talvez de certo"},
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/projetos", getprojetos)
+    router.GET("/projetos/:id", getprojetoByID)
+    router.POST("/projetos", postprojetos)
+
+    router.Run("localhost:8080")
+}
+
+// getprojetos responds with the list of all projetos as JSON.
+func getprojetos(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, projetos)
+}
+
+// postprojetos adds an projeto from JSON received in the request body.
+func postprojetos(c *gin.Context) {
+    var newprojeto projeto
+
+    // Call BindJSON to bind the received JSON to
+    // newprojeto.
+    if err := c.BindJSON(&newprojeto); err != nil {
+        return
+    }
+
+    // Add the new projeto to the slice.
+    projetos = append(projetos, newprojeto)
+    c.IndentedJSON(http.StatusCreated, newprojeto)
+}
+
+// getprojetoByID locates the projeto whose ID value matches the id
+// parameter sent by the client, then returns that projeto as a response.
+func getprojetoByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop through the list of projetos, looking for
+    // an projeto whose ID value matches the parameter.
+    for _, a := range projetos {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "projeto not found"})
+}
