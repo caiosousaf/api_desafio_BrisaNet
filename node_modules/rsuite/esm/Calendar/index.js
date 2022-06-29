97105ce470b0e7bf1b7f07aa5e6da6974cc24c52
PedroMiguel7 <93842972+PedroMiguel7@users.noreply.github.com>
2022-06-29 19:20:08 -0300

    Restruturando o front

--- node_modules/rsuite/esm/Calendar/index.js
+++
@@ -1,5 +0,0 @@
-import CalendarPanel from './CalendarPanel';
-import Calendar, { CalendarState } from './Calendar';
-import CalendarContext from './CalendarContext';
-export { Calendar, CalendarContext, CalendarState };
-export default CalendarPanel;
