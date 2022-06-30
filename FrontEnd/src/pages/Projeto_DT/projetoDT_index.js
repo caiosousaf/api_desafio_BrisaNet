46b19b14b7df26d60a619217b53e5c7697aea44e
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-06-30 15:46:42 -0300

    atualização

--- FrontEnd/src/pages/Projeto_DT/projetoDT_index.js
+++ FrontEnd/src/pages/Projeto_DT/projetoDT_index.js
@@ -7,11 +7,11 @@ const projetoDT_index = () => {
                 <Header />
                 <div className="row gap-3">
                     <div className="CardDT InfoProjeto row">
-                        <div className="col-5">
+                        <div className="col-12 col-lg-5">
                             <h4>Descrição</h4>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
                         </div>
-                        <div className="col-6">
+                        <div className="col-12 col-lg-6">
                             <div>
                                 <div>
                                     <h4 className="text-center">Funções</h4>
@@ -48,6 +48,34 @@ const projetoDT_index = () => {
                                         </div>
                                     </div>
                                     
+                                </div>
+
+                                <div className="row">
+                                    <div className="col">
+                                        Gráfico
+                                    </div>
+                                    <div className="Resumo col">
+                                        <div className="TotColaboradores row">
+                                            <h6 className="col d-flex justify-content">11</h6>
+                                            <p className="col">Total de Colaboradores</p>
+                                        </div>
+                                        <div className="row">
+                                            <div className="TotTarefas col d-flex flex-column align-items-center justify-content-center">
+                                                <h6>60</h6>
+                                                <p>Total de Tarefas</p>
+                                            </div>
+                                            <div className=" col">
+                                                <div className="TarefasAnd row">
+                                                    <h6>12</h6>
+                                                    <p>Tarefa em Andamento</p>
+                                                </div>
+                                                <div className="TarefasConc row">
+                                                    <h6>48</h6>
+                                                    <p>Tarefas Concluídas</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>                        
