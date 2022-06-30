b4585b2036a95740673aedf1c31c0e9d203c5a16
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-06-30 10:14:09 -0300

    alterando nome de pastas dnvvv

--- FrontEnd/src/routes.js
+++ FrontEnd/src/routes.js
@@ -8,9 +8,9 @@ import Projeto_DT from ".//pages/Projeto_DT/projetoDT_index";
 import Equipes from ".//pages/Equipes/equipe_index"
 import Equipe_DT from ".//pages/Equipe_DT/equipesDT_index"
 import Pessoas from ".//pages/Pessoas/pessoa_index"
-import Pessoas_DT from ".//pages/Pessoas_DT/pessoaDT_index"
+import Pessoa_DT from ".//pages/Pessoa_DT/pessoaDT_index"
 import Tarefas from ".//pages/Tarefas/tarefa_index"
-import Tarefas_DT from ".//pages/tarefa_DT/tarefaDT_index"
+import Tarefa_DT from ".//pages/Tarefa_DT/tarefaDT_index"
 
 
 const Rout = () =>{
@@ -20,16 +20,16 @@ const Rout = () =>{
                 <Route exect path="/" element={<Menu/>}/>
 
                 <Route exect path="/projetos" element={<Projeto />}/>
-                <Route exect path="/projetos/:id" element={<Projeto_DT />}/>
+                <Route exect path="/projeto/:id" element={<Projeto_DT />}/>
 
                 <Route exect path="/equipes" element={<Equipes />}/>
                 <Route exect path="/equipe/:id" element={<Equipe_DT />}/>
 
                 <Route exect path="/pessoas" element={<Pessoas />}/>
-                <Route exect path="/pessoas/:id" element={<Pessoas_DT />}/>
+                <Route exect path="/pessoa/:id" element={<Pessoa_DT />}/>
 
                 <Route exect path="/tarefas" element={<Tarefas />}/>
-                <Route exect path="/tarefas/:id" element={<Tarefas_DT />}/>
+                <Route exect path="/tarefa/:id" element={<Tarefa_DT />}/>
                 
 
             </Routes>
