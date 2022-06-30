ece4a46d5be20139a7cd40d537a250b914fcd567
PedroMiguel7 <93842972+PedroMiguel7@users.noreply.github.com>
2022-06-30 14:06:01 -0300

    add gitignore +  delete node models

--- FrontEnd/node_modules/rsuite/cjs/Calendar/index.d.ts
+++
@@ -1,8 +0,0 @@
-import CalendarPanel from './CalendarPanel';
-import Calendar, { CalendarState } from './Calendar';
-import CalendarContext from './CalendarContext';
-import { CalendarContextValue } from './types';
-export { Calendar, CalendarContext, CalendarState };
-export type { CalendarContextValue };
-export type { CalendarPanelProps } from './CalendarPanel';
-export default CalendarPanel;
--- FrontEnd/node_modules/rsuite/cjs/utils/BrowserDetection.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const isIE: () => boolean;
-export declare const isIE10: () => boolean;
-export declare const isIE11: () => boolean;
-export declare const isEdge: () => boolean;
