eb3e853bc6e6bf2f91cb9adbceb69d6c604b46bd
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-07-01 16:28:39 -0300

    Update projetoDT_index.js

--- FrontEnd/src/pages/ProjetoDT/projetoDT_index.js
+++ FrontEnd/src/pages/ProjetoDT/projetoDT_index.js
@@ -12,7 +12,7 @@ const projetoDT_index = () => {
                     <div className="CardDT InfoProjeto row py-4">
                         <div className="col-12 col-lg-5 me-4">
                             <h4>Descrição</h4>
-                            <p style={{textAlign: 'justify', fontWeight: 300}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+                            <p style={{textAlign: 'justify', fontWeight: 300, lineHeight: '1.6em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
                         </div>
                         <div className="col-12 col-lg-6 ms-5">
                             <div>
@@ -98,7 +98,7 @@ const projetoDT_index = () => {
                             <Table />
                         </div>
                         <div className="CardDT col">
-                            B
+                            <Table />
                         </div>
                     </div>
                        
