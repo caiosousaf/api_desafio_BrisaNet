8bed2a6d7c7b0edd5ee36f2eaf3d27e4fefc487b
PedroMiguel7 <93842972+PedroMiguel7@users.noreply.github.com>
2022-06-30 22:54:42 -0300

    add card lateral home direito

--- FrontEnd/src/pages/Home/home.js
+++ FrontEnd/src/pages/Home/home.js
@@ -57,44 +57,35 @@ function Menu_index() {
                     </a>
                 </div>
             </div>
-          </div>
+            </div>
 
           <div className="col-4 ms-3">
             <div
-              className="cardlateraldireito col-lg-3 col-md-12 p-4 mt-4"
+              className="cardLateralHome col-lg-3 col-md-12 p-4 mt-5 "
               style={{ width: "100%" }}
             >
-              <div className="cardLateralHome d-flex justify-content-around ">
-                <div>
-                    <div className="d-center">
-                        <h1><span className="azulzinhoClaro">60</span></h1>
+                <div className="Resumo col-md-12 col-lg-6 justify-content-center">
+                    <div className="TotColaboradores row d-flex align-items-center justify-content-center">
+                        <h6 className="col-3 d-flex justify-content-end">11</h6>
+                        <p className="col-5">Total de Colaboradores</p>        
                     </div>
-                    <div>
-                        total de projetos
-                    </div>
-
-                </div>
-                <div className="linhaVertical"></div>
-                <div>
-                    <div>
-                        <div>
-                        <h1><span className="">10</span></h1>
+                    <div className="row">
+                        <div className="TotTarefas col d-flex flex-column align-items-center justify-content-center">
+                            <h6 className="col">60</h6>
+                            <p className="text-center col">Total de Tarefas</p>
                         </div>
-                        <div>
-                            em desenvolvimento
-                        </div>
-                    </div>
-                    <div>
-                    <div>
-                        <h1><span className="">50</span></h1>
-                        </div>
-                        <div>
-                            concluidos
+                        <div className="col d-flex flex-column align-items-center justify-content-center">
+                            <div className="TarefasAnd row d-flex align-items-center justify-content-center">
+                                <h6 className="col-4">12</h6>
+                                <p className="col-7">Tarefa em Andamento</p>
+                            </div>
+                            <div className="TarefasConc row d-flex align-items-center justify-content-center">
+                                <h6 className="col-4">48</h6>
+                                <p className="col-7">Tarefas Concluídas</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                
-              </div>
             </div>
           </div>
         </div>
