d7f453672c02aaa23e3fd1c34240a825a46e4c8c
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-07-01 08:15:21 -0300

    colocando link para cards de projetos

--- FrontEnd/src/pages/Projetos/projeto_index.js
+++ FrontEnd/src/pages/Projetos/projeto_index.js
@@ -7,37 +7,84 @@ function ProjetoIndex() {
     return (
         <>
             <main className='col-11 offset-1 col-lg-11 offset-lg-1 px-5'>
-                <Header titulo="Projetos"/>
+                <Header titulo="Projetos" />
 
                 <div className="Options row my-lg-4 my-3">
                     <div className="LeftOptions col-lg-2 mt-sm-2">
                         <span className="me-2">Show:</span>
-                        <input type="" name="txt-show" id="txt-show" size="1"/>
+                        <input type="" name="txt-show" id="txt-show" size="1" />
                     </div>
                     <div className="RightOptions col-lg-2 offset-lg-8 offset-md-8 mt-sm-2 mt-2">
                         <button className="new-project me-3">
-                        <img src={NewProject} alt=""/>
+                            <img src={NewProject} alt="" />
                         </button>
-                        
+
                         <select className="ps-1 me-3" name="order-select" id="order-select">
-                        <option value="crescente">A - Z</option>
-                        <option value="decrescente">Z - A</option>
+                            <option value="crescente">A - Z</option>
+                            <option value="decrescente">Z - A</option>
                         </select>
 
                         <button className="filter px-2 py-1">
-                        <span>Filter</span> <img src={filter} alt=""/>
+                            <span>Filter</span> <img src={filter} alt="" />
                         </button>
                     </div>
                 </div>
 
                 <div className="row CardsContainer my-4">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    <div className="d-flex">
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                    </div>
+                    <div className="d-flex">
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                    </div>
+                    <div className="d-flex">
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                        <a href={"projeto/:id"} className="text-reset text-decoration-none">
+                            {" "}
+                            <Card />
+                        </a>
+                    </div>
                 </div>
 
             </main>
