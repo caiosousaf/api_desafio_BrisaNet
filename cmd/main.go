eb61338e895ed6d3ffa131ee275c8c1cc83fcbba
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-11-07 09:38:19 -0300

    feat: Resolução URI

--- cmd/main.go
+++ cmd/main.go
@@ -31,3 +31,44 @@ func main() {
 
     r.Run(":"+port)
 }
+// URI
+// package main
+
+// import (
+//    "bytes"
+//    "fmt"
+// )
+
+// type slice struct {
+//    tok []float64
+// }
+
+// func (s *slice) Scan(state fmt.ScanState, verb rune) error {
+//    tok, err := state.Token(false, func(r rune) bool { return r != '\n' })
+//    if err != nil { return err }
+//    if _, _, err := state.ReadRune(); err != nil {
+//       if len(tok) == 0 {
+//          panic(err)
+//       }
+//    }
+//    b := bytes.NewReader(tok)
+//    for {
+//       var d float64
+//       _, err := fmt.Fscan(b, &d)
+//       if err != nil { break }
+//       s.tok = append(s.tok, d)
+//    }
+//    return nil
+// }
+
+// func main() {
+//    var s slice
+//    var f slice
+//    fmt.Scan(&s)
+//    fmt.Scan(&f)
+//    vPeca1 := s.tok[1] * s.tok[2]
+//    vPeca2 := f.tok[1] * f.tok[2]
+//    ValorTotal := vPeca1 + vPeca2
+//    fmt.Printf("VALOR A PAGAR: R$ %.2f\n",ValorTotal)
+
+// }
